9f289703c804a8a848b8cd81b7d3d76ab88cc821
Sowrovksk <alluncn42@gmail.com>
2026-06-14 10:17:41 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import telebot
 import datetime
 
-TOKEN = "YOUR_BOT_TOKEN"
+# এখানে তোমার Token বসাবে (নিজে বসাবে)
+TOKEN = "8827195006:AAEjI6NbHMJWQKrr_kjCGMK4yzMykjaFeEw"
+
 ADMIN_ID = 6533813373
 
-bot = telebot.TeleBot(8827195006:AAEjI6NbHMJWQKrr_kjCGMK4yzMykjaFeEw)
+bot = telebot.TeleBot(TOKEN)
 
 orders = {}
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id,
-    "🚀 Welcome!\nSend your order link.")
+    "🚀 Welcome to Order Bot\n\nSend your service link to place order.")
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
@@ -19,16 +21,18 @@ def handle_message(message):
     text = message.text
 
     order_id = len(orders) + 1001
+
     orders[order_id] = {
         "user": user,
         "link": text,
-        "status": "Pending"
+        "status": "Pending",
+        "date": str(datetime.datetime.now())
     }
 
     bot.send_message(message.chat.id,
-    f"Order Received ✅\nID: #{order_id}")
+    f"✅ Order Received!\nOrder ID: #{order_id}\nStatus: Pending")
 
     bot.send_message(ADMIN_ID,
-    f"New Order\nID: #{order_id}\nUser: @{user}\nLink: {text}")
+    f"🔔 New Order\n\nID: #{order_id}\nUser: @{user}\nLink: {text}")
 
 bot.polling()
